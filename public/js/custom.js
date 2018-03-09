9e12c783941d77d8ea977832d2c9a9e1a095cb4a
minhngoc2512 <ngocnm@traithivang.vn>
2018-03-09 15:58:05 +0700

    add

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,3 +3,37 @@ $('.list-item').slick({
     arrows:false,
     autoplay:false
 });
+$('.list-item-film-3').slick({
+    dots:true,
+    arrows:false,
+    autoplay:false,
+    vertical:true
+});
+
+$('.list-item-film-3-2').slick({
+    dots:true,
+    arrows:false,
+    autoplay:false,
+    vertical:true
+});
+
+$('.slider-item').slick({
+    dots:false,
+    arrows:true,
+    autoplay:false,
+    prevArrow: '<button type="button" class="btn-slider-prev"><img src="public/images/icon/icon_prev_slider.png"></button>',
+    nextArrow:'<button type="button" class="btn-slider-next"><img src="public/images/icon/icon_next_slider.png"></button>'
+
+});
+//menu nav
+$('.dropdown-menu').hover(
+    function(){
+        $(this).addClass('open');
+    },function(){
+        $(this).removeClass('open');
+
+    }
+);
+if( $(".img-slider").hasClass("slick-active")){
+    console.log($("#slider-h .img-slider slick-active").attr("alt"));
+}
