914f4a99ff1f739416caf369d9f2563464e144bd
minhngoc2512 <ngocnm@traithivang.vn>
2018-03-12 16:03:32 +0700

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,6 +3,7 @@
 //   arrows: false,
 //   autoplay: true
 // });
+
 $('.list-item-group-1').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
@@ -12,6 +13,19 @@ $('.list-item-group-1').slick({
   focusOnSelect: true,
   arrows: false,
 });
+
+// $(".list-item-group-1").on("afterChange", function(slick, currentSlide) {
+//   var str = $(".list-item-group-1").slick('getSlick');  
+//   var tag_html = $(".list-item-group-1 .slick-list .item.slick-slide.slick-active");
+//   console.log(tag_html.length);
+//   for(var i=0;i<tag_html.length;i++){
+//     // tag_html[i].css('width','188px!important');
+//   }
+  
+// });
+
+
+
 $('.list-item-group-2').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
@@ -37,20 +51,29 @@ $(".item-4-2").slick({
 });
 
 $(".slider-item").slick({
+  // slidesToShow: 1,
+  // slidesToScroll: 1,
+  // lazyLoad: 'ondemand',
   dots: false,
   arrows: false,
   autoplay: true
   // prevArrow: '<button type="button" class="btn-slider-prev"><img src="public/images/icon/icon_prev_slider.png"></button>',
   // nextArrow:'<button type="button" class="btn-slider-next"><img src="public/images/icon/icon_next_slider.png"></button>'
 });
+
+// $('.lazy').slick({
+//   lazyLoad: 'ondemand',
+//   slidesToShow: 3,
+//   slidesToScroll: 1
+// });
 //menu nav
-$(".dropdown-menu").hover(
+$(".dropdown-menu").stop().hover(
     function(){
         var id = $(this).attr('data-id');
-        $('#menu-child-nav-'+id).show(500);
+        $('#menu-child-nav-'+id).stop().show(500);
     },function(){
         var id = $(this).attr('data-id');
-        $('#menu-child-nav-'+id).hide(500);
+        $('#menu-child-nav-'+id).stop().hide(500);
     }
 );
   // function() {
